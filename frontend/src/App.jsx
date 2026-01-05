967d889fb30c28f980f7d3a04aaab84393973c28
Sanjana-Bhandari <kumarisanjana.work@gmail.com>
2026-01-05 16:25:56 +0545

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import Header from "./components/Header";
+import TopNewsBar from "./components/TopNewsBar";
+import MainNews from "./components/MainNews";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
-      <Header/>
-     
+    <div className="App">
+      <Header />
+      <TopNewsBar />
+      <MainNews />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
